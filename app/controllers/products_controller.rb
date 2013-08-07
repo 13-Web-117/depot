bd62067c2aa67dc56c8c8f8027478cb5b7452e1a
kthsdwwl <kthsdwwl@gmil.com>
2013-08-07 10:03:09 +0800

    fix a bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  skip_before_filter :authorize, only: [:add_like]
   before_action :set_product, only: [:show, :edit, :update, :destroy, :add_like]
 
   # GET /products
